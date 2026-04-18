6faf91b33b2be165e912003a2f5a5b11749ec82b
Ryan <ryan@coretsu.com>
2026-04-18 17:49:52 -0400

    integrate cli for now

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import auth, deployments, health, uploads, stuff
+from app.api.routes import auth, deployments, health, uploads
 
 api_router = APIRouter()
 api_router.include_router(health.router, tags=["health"])
-api_router.include_router(stuff.router, prefix="/stuff", tags=["stuff"])
 api_router.include_router(auth.router)
 api_router.include_router(uploads.router)
 api_router.include_router(deployments.router)
